12db730fb69d55fe9886b43d432ee057533d300d
juhitiwari <tjuhi.321@gmail.com>
2020-05-18 22:15:07 +0530

    results

--- src/screens/VideoPlayer.js
+++ src/screens/VideoPlayer.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { View, Text,StyleSheet, Dimensions } from 'react-native';
 import Constants from 'expo-constants'
 import WebView from 'react-native-webview';
+import { useTheme } from '@react-navigation/native';
 const VideoPlayer=({route})=> {
     const {videoId,title}=route.params
- 
+    const {colors}=useTheme()
+    const mycolor=colors.iconColor
 
 
     return (
@@ -15,7 +17,7 @@ const VideoPlayer=({route})=> {
               source={{uri:`https://www.youtube.com/embed/${videoId}`}}/>
             
           </View>
-    <Text style={styles.title}>{title}</Text>
+    <Text style={[styles.title,{color:mycolor}]}>{title}</Text>
     <View style={{borderBottomWidth:1}}/>
         
       </View>
